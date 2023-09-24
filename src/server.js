40bfa8b5a294f970850a965416ff7996fbd24df2
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2023-09-24 17:07:38 +0700

    deploy-notes-app

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import Hapi from "@hapi/hapi";
+import routes from "../src/routes.js";
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: "localhost",
+        routes: {
+            cors: {
+                origin: ["*"],
+            },
+        },
+    });
+    server.route(routes);
+    await server.start();
+
+    console.log(`server running on port ${server.info.uri}..`);
+};
+
+init();
