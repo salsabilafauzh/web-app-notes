57dc90dcc373d1a80225a29933d76907fa0227d9
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2023-09-24 17:13:46 +0700

    fix bugs env

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
-import Hapi from "@hapi/hapi";
-import routes from "../src/routes.js";
+import Hapi from '@hapi/hapi';
+import routes from '../src/routes.js';
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: "localhost",
-        routes: {
-            cors: {
-                origin: ["*"],
-            },
-        },
-    });
-    server.route(routes);
-    await server.start();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+  await server.start();
 
-    console.log(`server running on port ${server.info.uri}..`);
+  console.log(`server running on port ${server.info.uri}..`);
 };
 
 init();
