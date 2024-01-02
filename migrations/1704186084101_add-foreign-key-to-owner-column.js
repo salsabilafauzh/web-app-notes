0b6a60c4788589dfa066c5ac1105a06c839261ec
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-01-02 16:33:26 +0700

    update migration file add foreign key to owner table

--- migrations/1704186084101_add-foreign-key-to-owner-column.js
+++ migrations/1704186084101_add-foreign-key-to-owner-column.js
@@ -8,3 +8,14 @@ exports.up = (pgm) => {
   // memberikan constraint foreign key pada owner terhadap kolom id dari tabel users
   pgm.addConstraint('notes', 'fk_notes.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
 };
+
+exports.down = (pgm) => {
+  // menghapus constraint fk_notes.owner_users.id pada tabel notes
+  pgm.dropConstraint('notes', 'fk_notes.owner_users.id');
+
+  // mengubah nilai owner old_notes pada note menjadi NULL
+  pgm.sql("UPDATE notes SET owner = NULL WHERE owner = 'old_notes'");
+
+  // menghapus user baru.
+  pgm.sql("DELETE FROM users WHERE id = 'old_notes'");
+};
