8fadd6bb560eb136d55b9ec39d4c49fdb3b35d46
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-01-16 16:06:08 +0700

    connection redis elasticache aws

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -3,9 +3,10 @@ const redis = require('redis');
 class CacheService {
   constructor() {
     this._client = redis.createClient({
-      socket: {
-        host: process.env.REDIS_SERVER,
-      },
+      //   socket: {
+      //     host: process.env.REDIS_SERVER,
+      //   },
+      host: process.env.REDIS_SERVER,
     });
     this._client.on('error', (error) => {
       console.error(error);
