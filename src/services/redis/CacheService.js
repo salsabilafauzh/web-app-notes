372883453e42e28fc5d533774861966ef78bbde4
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-01-19 12:52:53 +0700

    redis

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -3,8 +3,9 @@ const redis = require('redis');
 class CacheService {
   constructor() {
     this._client = redis.createClient({
-      host: process.env.REDIS_SERVER,
-      port: process.env.REDIS_PORT,
+      socket: {
+        host: process.env.REDIS_SERVER,
+      },
     });
     this._client.on('error', (error) => {
       console.error(error);
